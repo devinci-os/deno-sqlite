d3394dc8a662c6970e4a07a6611d9f56c6ca239c
Nikhil Saraf <nsaraf98@gmail.com>
2021-11-22 23:06:47 -0800

    Update sqlite.js

--- build/sqlite.js
+++ build/sqlite.js
@@ -4,7 +4,7 @@ import env from "./vfs.js";
 
 // Create wasm instance and seed random number generator
 export default async function instantiate(url) {
-  const module = await WebAssembly.compileStreaming(fetch(url));
+  const module = await WebAssembly.compileStreaming(await fetch(url));
   const placeholder = { exports: null };
   const instance = WebAssembly.Instance(module, env(placeholder));
   placeholder.exports = instance.exports;
